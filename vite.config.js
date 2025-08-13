0ae079dada39cc0865049db32d02e5119bb2a75e
NOHDAEFR <your-email@example.com>
2025-08-13 13:10:08 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MemoryGame/'
+  base: '/MarioGame/' // חשוב: חייב להתאים לשם הרפוזיטורי
 })
